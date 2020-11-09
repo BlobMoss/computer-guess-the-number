0970029568a4cbf5a6f0f7105c62abc4b2f55a74
Globfish <51151906+Globfish@users.noreply.github.com>
2020-11-09 15:58:43 +0100

    (WIP) can jump less than 1 which is pointless

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
 let guess
+let guessRange
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
+    guessRange /= 2
+    guess -= guessRange
+    updateMessage()
+})
 
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    // Vad ska hända när man tryckt correct?
+    setMessage("woho :D")
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
-    
+    guessRange /= 2
+    guess += guessRange
+    updateMessage()
 })
-
 
 function setMessage(msg) {
     document.getElementById('message').innerText = msg
@@ -22,9 +31,14 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
+    guess = MAX_NUMBER / 2
+    guessRange = guess;
 
-    // setMessage(`Is it ${guess}?`)
+    updateMessage()
+}
+
+function updateMessage(){
+    setMessage(`Is it ${Math.floor(guess)}?`)
 }
 
 document
